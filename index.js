65d6df75d6376a48549082d750b7e1fc5ec7a0eb
Chris <chris.mahfouz@hotmail.com>
2021-02-19 15:54:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function iterativeLog(array){
-  array.forEach(index,element)
-  console.log(${index}: ${element})
+  array.forEach((index,element) => {
+    console.log(`${index}: ${element}`)
+  })
+  
 
 }
